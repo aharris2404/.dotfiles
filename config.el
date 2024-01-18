a5ee4ee1456a29f9674e1eef5ba435b69a8e9a8b
Andrew Harris <andrewharris@andrews-mbp.mynetworksettings.com>
2024-01-18 17:11:45 -0500

    stable with theme change

--- config.el
+++ config.el
@@ -18,24 +18,34 @@
 ;; - `doom-symbol-font' -- for symbols
 ;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
 ;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
+;; see 'c-h v doom-font' for documentation and more examples of what they
+;; accept. for example:
 ;;
-(setq doom-font (font-spec :family "Source Code Pro" :size 16 :weight 'medium))
-;;(setq doom-font (font-spec :family "Fira Code" :size ))
+(setq doom-font (font-spec :family "monaco" :size 16 :weight 'medium))
+;;(setq doom-font (font-spec :family "fira code" :size ))
 ;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;(setq doom-font (font-spec :family "fira code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "fira sans" :size 13))
 ;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
+;; if you or emacs can't find your font, use 'm-x describe-font' to look them
+;; up, `m-x eval-region' to execute elisp code, and 'm-x doom/reload-font' to
+;; refresh your font settings. if emacs still can't find your font, it likely
+;; wasn't installed correctly. font issues are rarely doom issues!
 
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+;; there are two ways to load a theme. both assume the theme is installed and
+;; available. you can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. this is the default:
+(setq doom-theme 'doom-opera)
+
+(custom-theme-set-faces! 'doom-opera
+  '(line-number :foreground "dim gray")
+  '(line-number-current-line :foreground "white")
+  ;; '(doom-modeline-project-dir :foreground "blue")
+  ;; '(doom-modeline-buffer-path   :foreground "blue")
+  ;;'(mode-line-inactive :background "dim gray" :foreground "white" :height 80)
+  ;;'(mode-line :background "black")
+  ;;'(default :background "#002b36")
+  )
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
