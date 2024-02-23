df62ac9601f66df61ce86cb0c86c4c53303ee2c3
Andrew Harris <haran5l@p3747p2j9q.uswin.ad.vzwcorp.com>
2024-02-23 12:47:00 -0500

    vz mac first commit

---
+++ .doom.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(org-agenda-files
+   '("~/org/2.19.24.org" "/Users/haran5l/org/emacs_config.org" "/Users/haran5l/org/2.12.24.org" "/Users/haran5l/org/2.5.24.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
