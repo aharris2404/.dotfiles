44b65c2697ac2ead90480eaf852251e09a05f85d
Andrew <andrew.harris2404@gmail.com>
2024-04-25 00:20:47 -0400

    added my-shell-functions

--- .doom.d/machine/vz-mac.el
+++ .doom.d/machine/vz-mac.el
@@ -7,3 +7,15 @@
       )
 
 (message "Successfully loaded VZ-mac configuration!")
+
+(defun my-run-in-inferior-shell (input)
+    (interactive "sInput: ")
+    (comint-send-string "*shell*" (concat input "\n")))
+
+(defun my-git-pull-in-inferior-shell (remote branch)
+  (interactive "sRemote: \nsBranch: ")
+    (comint-send-string "*shell*" (format "git push %s %s\n" remote branch)))
+
+(defun my-git-push-in-inferior-shell (remote branch)
+  (interactive "sRemote: \nsBranch: ")
+    (comint-send-string "*shell*" (format "git push %s %s\n" remote branch)))
