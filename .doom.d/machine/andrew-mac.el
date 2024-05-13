dcbd2f6b9e7e4d8504ba7d4c50b96123c347dd14
Andrew <andrew.harris2404@gmail.com>
2024-05-13 09:48:38 -0400

    added redshift-conf

--- .doom.d/machine/andrew-mac.el
+++ .doom.d/machine/andrew-mac.el
@@ -1,3 +1,7 @@
 ;;; ../.dotfiles/.doom.d/machine/andrew-mac.el -*- lexical-binding: t; -*-
 
 (message "Successfully loaded personal configuration!")
+
+(defun redshift-on ()
+  (interactive)
+  (async-shell-command "redshift -l 40:-74 -c ~/redshift.conf" "*redshift*"))
